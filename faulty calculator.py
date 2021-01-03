66694798a55c8eb56f3fba66854d50999bf04a74
saurabhmahapurkar <71888989+saurabhmahapurkar@users.noreply.github.com>
2021-01-03 21:47:53 +0530

    Add files via upload

--- faulty calculator.py
+++ faulty calculator.py
@@ -1,4 +1,42 @@
-#a=int(input("enter the operation"))
-#b=int(input("enter the second no"))
+print('Welcome to the calculator world!!')
+print('Please type the math operation you want to complete')
+print("+ for addition \n - for subraction \n * for multiplication \n / for division \n ** for power \n % for modulus")
+while(True):
+    num1 = int(input('Enter the first number: '))
+    num2 = int(input('Enter the second number: '))
+    operator = input('Which operator do you want to use?: ')
 
-0x35|0x75
+    if operator == '+':
+        if num1 == 56 and num2 == 9:
+            print(77)
+        else:
+            print(num1 + num2)
+
+    if operator == '-':
+        print(num1 - num2)
+
+    if operator == '*':
+        if num1 == 45 and num2 == 3:
+            print(555)
+        else:
+            print(num1 * num2)
+
+    if operator == '/':
+        if num1 == 56 and num2 == 6:
+            print(4)
+        else:
+            print(num1 / num2)
+
+    if operator == '**':
+        print(num1 ** num2)
+
+    if operator == '%':
+        print(num1 % num2)
+
+    again = input(
+        'Do you want to use Calculator again? \n Press y for yes and n for no: ')
+    if again == 'y':
+        continue
+    else:
+        print('See you again!')
+        break
